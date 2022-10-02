f4bef811c290fd4ab6d6cc51d472d83f046a7b41
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2022-10-02 13:46:22 +0500

    add image assets

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
         path: path.resolve(__dirname,'dist/'),
         filename: '[name][contenthash].js',
         clean: true,
+        assetModuleFilename: '[name][ext]',
     },
     devtool: 'source-map',
     devServer: {
@@ -27,6 +28,10 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
             },
         ],
     },
